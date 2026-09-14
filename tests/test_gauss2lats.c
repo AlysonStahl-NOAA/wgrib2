e2ede20621a40c3ff1411cd515bb7c88377ad6d0
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-09-14 18:15:56 -0500

    test gord()

--- tests/test_gauss2lats.c
+++ tests/test_gauss2lats.c
@@ -1,7 +1,8 @@
 /**
  * This is a test for the wgrib2 project. 
  * 
- * This test is for the gauss2lats() routine from geo.c.
+ * This test is for the gauss2lats() routine and it's helper 
+ * function gord() from geo.c.
  * Alyson Stahl, 9/2026
  */
 
@@ -12,12 +13,24 @@
 
 #define TOL 1e-6
 
+double gord(int n, double x);
+
 extern enum output_order_type output_order;
 
 int
 main(){
-    printf("Testing gauss2lats()...\n");
+    printf("Testing gord()...\n");
     {
+        int n = 2;
+        double x = 0.5;
+        double expected = -0.19764235376052372;
+        double ret;
+
+        ret = gord(n, x);
+        if (fabs(ret - expected) > TOL) {
+            printf("gord() returned an unexpected value.\n");
+            return 1;
+        }
 
     }
     printf("SUCCESS!\n");
