860696d18c458073ee14816ef343b2a3cd3e1f42
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-09-14 19:09:41 -0500

    another case

--- tests/test_gauss2ll.c
+++ tests/test_gauss2ll.c
@@ -92,20 +92,6 @@ main(){
     }
     printf("Test Case 2: Quasi-regular Gaussian lat-lon grid.\n");
     {
-        /**
-         * TODO: Replace the ??? below with code that sets up the input and output
-         * for the gauss2ll() function. The intention of the input values selected
-         * is to execute the "happy path" of the routine successfully. The values 
-         * chosen should reflect typical, valid input values for the function and should
-         * also result in as many "branches" of the code being executed as possible. Unlike the previous
-         * case (test case 1), this case should cover quasi-regular grids. In other words, the following if 
-         * statement should execute as FALSE, executing the else branch:
-         * if (nnx_ > 0)
-         * 
-         * You are only providing function input, output, and expected value SETUP here. Do not call
-         * the function under test or implement any assertions / expected value checks.
-         * Do not add any unneccessary "scope" around the setup code.
-         */
         unsigned char sec1[16] = {0};
         unsigned char sec3[74] = {0};
         unsigned char *sec[8] = {NULL};
@@ -173,6 +159,75 @@ main(){
                 free(llat);
                 free(llon);
                 return 2;
+            }
+        }
+
+        free(llat);
+        free(llon);
+    }
+    printf("Test Case 3: Regular Gaussian lat-lon grid with negative x and y scanning order.\n");
+    {
+        unsigned char sec1[16] = {0};
+        unsigned char sec3[72] = {0};
+        unsigned char *sec[8] = {NULL};
+        double *llat = NULL;
+        double *llon = NULL;
+        unsigned int exp_size = 6;
+        const double expected_lat[6] = {
+            19.875719147440904, 19.875719147440904, 19.875719147440904,
+            59.444408289166775, 59.444408289166775, 59.444408289166775
+        };
+        const double expected_lon[6] = {350.0, 0.0, 10.0, 350.0, 0.0, 10.0};
+
+        sec[1] = sec1;
+        sec[3] = sec3;
+
+        sec1[3] = 16;
+        sec1[4] = 1;
+
+        sec3[3] = 72;
+        sec3[4] = 3;
+        sec3[9] = 6;
+        sec3[13] = 40;
+        sec3[33] = 3;
+        sec3[37] = 2;
+        sec3[46] = 3;
+        sec3[47] = 139;
+        sec3[48] = 12;
+        sec3[49] = 184;
+        sec3[50] = 0;
+        sec3[51] = 152;
+        sec3[52] = 150;
+        sec3[53] = 128;
+        sec3[55] = 1;
+        sec3[56] = 47;
+        sec3[57] = 71;
+        sec3[58] = 135;
+        sec3[59] = 20;
+        sec3[60] = 220;
+        sec3[61] = 147;
+        sec3[62] = 128;
+        sec3[63] = 0;
+        sec3[64] = 152;
+        sec3[65] = 150;
+        sec3[66] = 128;
+        sec3[70] = 2;
+        sec3[71] = 128;
+
+        if (gauss2ll(sec, &llat, &llon) != 0 || llat == NULL || llon == NULL) {
+            printf("gauss2ll() failed on valid input.\n");
+            free(llat);
+            free(llon);
+            return 3;
+        }
+
+        for (unsigned int i = 0; i < exp_size; i++) {
+            if (fabs(llat[i] - expected_lat[i]) > TOL ||
+                fabs(llon[i] - expected_lon[i]) > TOL) {
+                printf("gauss2ll() produced an unexpected coordinate at index %u.\n", i);
+                free(llat);
+                free(llon);
+                return 3;
             }
         }
 
